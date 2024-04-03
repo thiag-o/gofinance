b552fa0870e3c85636d4d918f4d200c890acd90e
Thiago Borges Mansano <thiago@clubeagro.com.br>
2024-04-02 22:15:06 -0300

    feat: adicionar fonts para o app e tema

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import * as SplashScreen from 'expo-splash-screen';
+
 import theme from './src/global/styles/theme';
 import { Dashboard } from './src/screens/Dashboard';
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Dashboard />
-    </ThemeProvider>
-  );
+  const [fontsLoaded, fontsError] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded || fontsError) {
+    SplashScreen.preventAutoHideAsync();
+    return null;
+  }
+  SplashScreen.hideAsync();
+  if (fontsLoaded) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Dashboard />
+      </ThemeProvider>
+    );
+  }
 }
