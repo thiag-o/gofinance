66a99ce38a66f865c569abaca06e550c347c3bab
Thiago Borges Mansano <thiago@clubeagro.com.br>
2024-04-06 18:38:36 -0300

    feat: adicionar lista de cartoes de transacao

--- src/components/TransactionCard/index.tsx
+++ src/components/TransactionCard/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import { Container, Title, Amount, Footer, Category, Icon, CategoryName, Date } from './styles';
+import { Feather } from '@expo/vector-icons';
+interface Category {
+  name: string;
+  icon: string;
+}
+interface Data {
+  title: string;
+  amount: string;
+  category: Category;
+  date: string;
+}
 
-export function TransactionCard() {
+interface Props {
+  data: Data;
+}
+
+export function TransactionCard({ data }: Props) {
   return (
     <Container>
-      <Title>Desenvolvimento de site</Title>
-      <Amount>R$ 12.000,00</Amount>
+      <Title>{data.title}</Title>
+      <Amount>{data.amount}</Amount>
       <Footer>
         <Category>
-          <Icon name="dollar-sign" />
-          <CategoryName>Vendas</CategoryName>
+          <Icon name={data.category.icon as any} />
+          <CategoryName>{data.category.name}</CategoryName>
         </Category>
-        <Date>13/04/2020</Date>
+        <Date>{data.date}</Date>
       </Footer>
     </Container>
   );
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -16,9 +16,31 @@ import {
   HighLightCards,
   Transactions,
   Title,
+  TransactionList,
 } from './styles';
 
 export function Dashboard() {
+  const data = [
+    {
+      title: 'Desenvolvimento de site',
+      amount: 'R$ 12.000,00',
+      category: { name: 'Vendas', icon: 'dollar-sign' },
+      date: '13/04/2022',
+    },
+    {
+      title: 'Desenvolvimento de site',
+      amount: 'R$ 12.000,00',
+      category: { name: 'Vendas', icon: 'dollar-sign' },
+      date: '13/04/2022',
+    },
+    {
+      title: 'Desenvolvimento de site',
+      amount: 'R$ 12.000,00',
+      category: { name: 'Vendas', icon: 'dollar-sign' },
+      date: '13/04/2022',
+    },
+  ];
+
   return (
     <Container>
       <Header>
@@ -56,7 +78,14 @@ export function Dashboard() {
 
       <Transactions>
         <Title>Listagem</Title>
-        <TransactionCard />
+        <TransactionList
+          data={data}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: 20,
+          }}
+        />
       </Transactions>
     </Container>
   );
