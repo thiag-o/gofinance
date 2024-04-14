2348c779d7b1e0bcbf02a996a19be74f620e7cf4
Thiago Borges Mansano <thiago@clubeagro.com.br>
2024-04-14 19:11:55 -0300

    feat: adicionar splashScreen e icone

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "intl";
 
 import { ThemeProvider } from "styled-components";
@@ -17,7 +17,9 @@ import theme from "./src/global/styles/theme";
 import { Routes } from "./src/routes";
 import { AuthProvider, useAuth } from "./src/hooks/auth";
 
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [fontsLoaded, fontsError] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
@@ -26,22 +28,30 @@ export default function App() {
 
   const { userStorageLoading } = useAuth();
 
-  if (!fontsLoaded || fontsError || userStorageLoading) {
-    SplashScreen.preventAutoHideAsync();
+  useEffect(() => {
+    if (!fontsLoaded && !fontsError && !userStorageLoading) {
+      setAppIsReady(true);
+    }
+  }, [fontsLoaded, fontsError, userStorageLoading]);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
     return null;
   }
 
-  SplashScreen.hideAsync();
-  if (fontsLoaded) {
-    return (
-      <GestureHandlerRootView style={{ flex: 1 }}>
-        <ThemeProvider theme={theme}>
-          <StatusBar style="light" />
-          <AuthProvider>
-            <Routes />
-          </AuthProvider>
-        </ThemeProvider>
-      </GestureHandlerRootView>
-    );
-  }
+  return (
+    <GestureHandlerRootView onLayout={onLayoutRootView} style={{ flex: 1 }}>
+      <ThemeProvider theme={theme}>
+        <StatusBar style="light" />
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
+  );
 }
